6110e839aadba1d404d65d7a62ee128d122b797b
Daniil Maksymenko <konor00792@gmail.com>
2019-11-23 02:06:31 +0200

    Chat statistics first draft

--- app/main/service/chat_stats_service.py
+++ app/main/service/chat_stats_service.py
@@ -3,7 +3,7 @@ from datetime import date
 from datetime import datetime
 from datetime import time
 from functools import reduce
-import io
+import io, string
 from json import loads
 from re import sub
 from statistics import mean
@@ -36,7 +36,12 @@ def get_chat_statistics(token, chat_id):
     chat_info = get_chat_info(token, chat_id)
     messages = get_chat_messages(token, chat_id)
     
-    num_mess = len(messages)
+    nm_mess = len(messages)
+    temp_nm_mess = nm_mess
+
+    if nm_mess == 0:
+        nm_mess = 1
+
     num_users = len(chat_info['users'])
     days = count_days(chat_info['date_created'])
 
@@ -45,35 +50,82 @@ def get_chat_statistics(token, chat_id):
     for d in grouped:
         result.update(d)
     
+    act_num = 0
+
     for k, v in result.items():
         result[k] = {
             'username': k,
             'mean_char': mean_char(v),
-            'mean_word': mean_word(v)
+            'mean_word': mean_word(v),
+            'num_mess': num_mess(v),
+            'num_words': num_words(v),
+            'num_chars': num_chars(v),
+            'percent': num_mess(v) / nm_mess * 100
         }
+        
+        if result[k]['percent'] > 5.0:
+            act_num += 1
+
+    for user in chat_info['users']:
+        if not result.get(user['username']):
+            result[user['username']] = {
+                'username': user['username'],
+                'mean_char': 0,
+                'mean_word': 0,
+                'num_mess': 0,
+                'num_words': 0,
+                'num_chars': 0,
+                'percent': 0,
+                'days_in': count_days(user['date_joined'])
+            }
+        else:
+            result[user['username']]['days_in'] = count_days(user['date_joined'])
+
+    if temp_nm_mess == 0:
+        nm_mess = 0
 
     return {
-        'num_mess': num_mess,
+        'num_mess': nm_mess,
         'num_users': num_users,
         'days': days,
         'mean_mess_char_length': mean_char(messages),
         'mean_mess_word_length': mean_word(messages),
+        'num_act_users': act_num,
+        'num_afk_users': num_users - len(grouped),
         'grouped': result
-    }
+    }, 200
+
+
+def num_mess(messages):
+    return len(messages)
+
+
+def num_words(messages):
+    return len(' '.join(messages).split(' '))
+
+
+def num_chars(messages):
+    return len(''.join(messages))
 
 
 def mean_char(messages):
     try:
         return mean([len(message['content']) for message in messages])
     except:
-        return mean([len(message) for message in messages])
+        try:
+            return mean([len(message) for message in messages])
+        except:
+            return 0
 
 
 def mean_word(messages):
     try:
         return mean([len(message['content'].split(' ')) for message in messages])
     except:
-        return mean([len(message.split(' ')) for message in messages])
+        try:
+            return mean([len(message.split(' ')) for message in messages])
+        except:
+            return 0
 
 
 def time_to_update(update_time):
